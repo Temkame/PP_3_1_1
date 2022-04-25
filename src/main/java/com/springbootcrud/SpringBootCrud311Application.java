0bf69e53aac370d85e1642945716c74ac6ab437a
temkame <temkame@yandex.ru>
2022-04-25 19:17:11 +0300

    Initial commit

---
+++ src/main/java/com/springbootcrud/SpringBootCrud311Application.java
@@ -0,0 +1,13 @@
+package com.springbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrud311Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrud311Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/springbootcrud/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.springbootcrud.dao;
+
+import com.springbootcrud.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void addUser(User user);
+
+    void deleteUser(Long id);
+
+    void editUser(User user);
+
+    User getUserById(Long id);
+
+    List<User> getAllUsers();
+}
